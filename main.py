02cf8a62a803a61a9fe23d4758a3a9e28d562cc8
aimspaceflip <pravonablnt@gmail.com>
2024-04-04 15:38:05 +0300

    stable version.. refactoring for oop..

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from utils.logger import Logger
 
 
 async def start_bot():
-    # logger = Logger(filename='bot_collect_clients.log').get_logger()
-    logger = Logger().get_logger()
+    logger = Logger(filename='bot_collect_clients.log').get_logger()
+    #logger = Logger().get_logger()
     db_manager = DatabaseManager(logger)
     await db_manager.create_pool()
     my_bot = MyBot(BOT_TOKEN, db_manager, logger)
